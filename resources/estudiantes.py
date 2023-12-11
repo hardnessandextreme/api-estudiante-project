4fbcb73d3c3f2c657326ed5a5e7914845c29a810
Jonathan Quinté <86927718+hardnessandextreme@users.noreply.github.com>
2023-12-10 19:53:25 -0500

    Refactor estudiantes.py: Remove unused code and endpoints

--- resources/estudiantes.py
+++ resources/estudiantes.py
@@ -1,55 +1,6 @@
 from flask_restful import Resource, reqparse
 from models import EstudianteModel, CiudadModel
 
-class IndexResource(Resource):
-    def get(self):
-        return {'message': 'Bienvenido a la API de estudiantes',
-                'autor': 'Jonathan Quinte',
-                'endpoints': {
-                    'estudiantes': '/estudiantes/',
-                    'estudiante': '/estudiantes/<int:id_est>',
-                    'ciudades': '/ciudades/',
-                    'ciudad': '/ciudades/<int:id_ciudad>'
-                }}
-
-class CiudadesResource(Resource):
-    def get(self):
-        ciudades = CiudadModel.query.all()
-        result = []
-
-        for ciudad in ciudades:
-            result.append(ciudad.json())
-
-        return {'ciudades': result}
-
-    def post(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('nombre_ciudad', type=str, required=True)
-        data = parser.parse_args()
-
-        ciudad = CiudadModel(**data)
-        ciudad.save_to_db()
-
-        return ciudad.json(), 201
-
-class CiudadResource(Resource):
-    def get(self, id_ciudad):
-        ciudad = CiudadModel.query.filter_by(id_ciudad=id_ciudad).first()
-        if ciudad:
-            return ciudad.json()
-        return {'message': 'Ciudad no encontrada'}, 404
-
-    def put(self, id_ciudad):
-        parser = reqparse.RequestParser()
-        parser.add_argument('nombre_ciudad', type=str, required=True)
-        data = parser.parse_args()
-
-        ciudad = CiudadModel.query.filter_by(id_ciudad=id_ciudad).first()
-        if ciudad:
-            ciudad.nombre_ciudad = data['nombre_ciudad']
-            ciudad.save_to_db()
-            return ciudad.json()
-        return {'message': 'Ciudad no encontrada'}, 404
 
 class EstudiantesResource(Resource):
     def get(self):
